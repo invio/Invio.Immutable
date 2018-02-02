f3351c42b7d022b571e5587ddd38a1bfdbe4aa38
Brian Caruso <carusobp@gmail.com>
2018-02-02 13:07:58 -0800

    Create 'SetPropertyHandler' to standardize unordered IEnumerable property value comparisons

---
+++ src/Invio.Immutable/SetPropertyHandler.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Reflection;
+using Invio.Extensions.Reflection;
+using Invio.Hashing;
+
+namespace Invio.Immutable {
+
+    /// <summary>
+    ///   This is the <see cref="IPropertyHandler" /> to use for properties that are
+    ///   of types that implement <see cref="IEnumerable" />. In order for two values
+    ///   to be considered equal, the items within the enumerable must be the same in
+    ///   quantity and value, but they do not need to be in the same order. String
+    ///   representations will look similar to an array in JSON notation.
+    /// </summary>
+    public sealed class SetPropertyHandler : EnumerablePropertyHandlerBase {
+
+        private Func<IEnumerable, IEnumerable, bool> arePropertyValuesEqual { get; }
+        private Lazy<Func<object, object, object>> lazySetEqualsFunc { get; }
+
+        /// <summary>
+        ///   Creates an instance of <see cref="SetPropertyHandler" /> that
+        ///   uses the items. but not those items' orders, found in the
+        ///   <see cref="IEnumerable" /> values stored in the provided
+        ///   <see cref="PropertyInfo" /> to determine equality, generate
+        ///   hash codes, and create external string representations.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="property" /> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   Thrown when <paramref name="property" /> does not have a
+        ///   <see cref="PropertyInfo.PropertyType" /> that implements
+        ///   <see cref="IEnumerable" />.
+        /// </exception>
+        public SetPropertyHandler(PropertyInfo property) : base(property) {
+            if (this.PropertyType.IsDerivativeOf(typeof(ISet<>)) ||
+                this.PropertyType.IsDerivativeOf(typeof(IImmutableSet<>))) {
+
+                this.lazySetEqualsFunc = new Lazy<Func<object, object, object>>(
+                    () =>
+                        this.PropertyType
+                            .GetMethod(nameof(ISet<Object>.SetEquals))
+                            .CreateFunc1()
+                );
+
+                this.arePropertyValuesEqual = this.AreSetsEqual;
+            } else {
+                this.arePropertyValuesEqual = this.AreEnumerablesEquivalent;
+            }
+        }
+
+        /// <summary>
+        ///   Generates a consistent, determinitic hash code using the hash codes of
+        ///   items. If two enumerables contain the same items but those items are in
+        ///   a different order, they will still return the same hash code.
+        /// </summary>
+        /// <remarks>
+        ///   This implementation does not remove duplicate values when calculating
+        ///   the hash code. It assumes that duplicates have been removed ahead of time,
+        ///   as traditional set implementations do not allow duplicates to exist,
+        ///   and we do not want to inject opinionated definitions of what is a
+        ///   duplicate at this point in time.
+        /// </remarks>
+        protected override int GetPropertyValueHashCodeImpl(IEnumerable propertyValue) {
+            return HashCode.FromSet(propertyValue);
+        }
+
+        /// <summary>
+        ///   Compares the two <see cref="IEnumerable" /> implementations
+        ///   stored on the properties for equality. This is done using the
+        ///   items, but not those items' orders, found within each
+        ///   property value's <see cref="IEnumerable" /> implementation.
+        /// </summary>
+        /// <remarks>
+        ///   <para>
+        ///     The enumerables are considered equal if they each contain the same
+        ///     number of items, and that each item in one <see cref="IEnumerable" />
+        ///     is equal with an item found within the other <see cref="IEnumerable" />.
+        ///     If one of the values is <c>null</c>, they are only considered equal
+        ///     if they are both <c>null</c>.
+        ///   </para>
+        ///   <para>
+        ///     This implementation does not remove duplicate values when calcilating
+        ///     equality. It assumes that duplicates have been removed ahead of time,
+        ///     as traditional set implementations do not allow duplicates to exist,
+        ///     and we do not want to inject opinionated definitions of what is a
+        ///     duplicate at this point in time.
+        ///   </para>
+        /// </remarks>
+        protected override bool ArePropertyValuesEqualImpl(
+            IEnumerable leftPropertyValue,
+            IEnumerable rightPropertyValue) {
+
+            return this.arePropertyValuesEqual(leftPropertyValue, rightPropertyValue);
+        }
+
+        private bool AreSetsEqual(IEnumerable left, IEnumerable right) {
+            return (bool)this.lazySetEqualsFunc.Value(left, right);
+        }
+
+        private bool AreEnumerablesEquivalent(IEnumerable left, IEnumerable right) {
+            var leftElementCounts = GetElementCounts(left, out int leftNullCount);
+            var rightElementCounts = GetElementCounts(right, out int rightNullCount);
+
+            if (leftNullCount != rightNullCount ||
+                leftElementCounts.Count != rightElementCounts.Count) {
+
+                return false;
+            }
+
+            foreach (var kvp in leftElementCounts) {
+                var leftElementCount = kvp.Value;
+
+                if (!rightElementCounts.TryGetValue(kvp.Key, out int rightElementCount)) {
+                    return false;
+                }
+
+                if (leftElementCount != rightElementCount) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static IDictionary<object, int> GetElementCounts(
+            IEnumerable enumerable,
+            out int nullCount) {
+
+            var dictionary = new Dictionary<object, int>();
+
+            nullCount = 0;
+
+            foreach (var element in enumerable) {
+                if (element == null) {
+                    nullCount++;
+                } else {
+                    int num;
+                    dictionary.TryGetValue(element, out num);
+                    num++;
+                    dictionary[element] = num;
+                }
+            }
+
+            return dictionary;
+        }
+
+    }
+
+}
---
+++ test/Invio.Immutable.Tests/SetPropertyHandlerTests.cs
@@ -0,0 +1,297 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using Invio.Xunit;
+using Xunit;
+
+namespace Invio.Immutable {
+
+    [UnitTest]
+    public sealed class SetPropertyHandlerTests : EnumerablePropertyHandlerTestsBase {
+
+        private static ISet<PropertyInfo> properties { get; }
+        private static Random random { get; }
+
+        static SetPropertyHandlerTests() {
+            var parent = typeof(FakeImmutable);
+
+            properties = ImmutableHashSet.Create(
+                parent.GetProperty(nameof(FakeImmutable.Array)),
+                parent.GetProperty(nameof(FakeImmutable.Enumerable)),
+                parent.GetProperty(nameof(FakeImmutable.Set)),
+                parent.GetProperty(nameof(FakeImmutable.ImmutableSet))
+            );
+
+            random = new Random();
+        }
+
+        public static IEnumerable<object[]> MatchingMemberData {
+            get {
+                yield return new object[] {
+                    nameof(FakeImmutable.Array),
+                    new object[] { "foo", "bar" },
+                    new string[] { "foo", "bar" }
+                };
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Enumerable),
+                    ImmutableList.Create<object>(String.Empty, 45m, DateTime.MinValue),
+                    new object[] { 45m, String.Empty, DateTime.MinValue }
+                };
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Set),
+                    new HashSet<string> { "12345", "Hello, World", "Bananas", "Bananas" },
+                    ImmutableHashSet.Create("12345", "Hello, World", "Bananas", "12345")
+                };
+
+                var guid = Guid.NewGuid();
+
+                yield return new object[] {
+                    nameof(FakeImmutable.ImmutableSet),
+                    ImmutableSortedSet.Create(guid, Guid.Empty),
+                    ImmutableSortedSet.Create(Guid.Empty, guid)
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(MatchingMemberData))]
+        public void GetPropertyValueHashCode_Matches(
+            String propertyName,
+            IEnumerable leftPropertyValue,
+            IEnumerable rightPropertyValue) {
+
+            // Arrange
+
+            var handler = this.CreateHandler(propertyName);
+            var leftFake = this.NextFake().SetPropertyValue(propertyName, leftPropertyValue);
+            var rightFake = this.NextFake().SetPropertyValue(propertyName, rightPropertyValue);
+
+            // Act
+
+            var leftHashCode = handler.GetPropertyValueHashCode(leftFake);
+            var rightHashCode = handler.GetPropertyValueHashCode(rightFake);
+
+            // Assert
+
+            Assert.Equal(leftHashCode, rightHashCode);
+        }
+
+        [Theory]
+        [MemberData(nameof(MatchingMemberData))]
+        public void ArePropertyValuesEqual_Matches(
+            String propertyName,
+            IEnumerable leftPropertyValue,
+            IEnumerable rightPropertyValue) {
+
+            // Arrange
+
+            var handler = this.CreateHandler(propertyName);
+            var leftFake = this.NextFake().SetPropertyValue(propertyName, leftPropertyValue);
+            var rightFake = this.NextFake().SetPropertyValue(propertyName, rightPropertyValue);
+
+            // Act
+
+            var areEqual = handler.ArePropertyValuesEqual(leftFake, rightFake);
+
+            // Assert
+
+            Assert.True(areEqual);
+        }
+
+        public static IEnumerable<object[]> ArePropertyValuesEqual_MismatchedValues_MemberData {
+            get {
+
+                // Case of 'foo' differs
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Array),
+                    new object[] { "foo", "bar" },
+                    new string[] { "FOO", "bar" }
+                };
+
+                // DateTime.MinValue vs. DateTime.MaxValue
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Enumerable),
+                    ImmutableList.Create<object>(String.Empty, 45m, DateTime.MinValue),
+                    new object[] { String.Empty, 45m, DateTime.MaxValue }
+                };
+
+                // 12345 => 54321
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Set),
+                    new HashSet<string> { "12345", "Hello, World", "Bananas" },
+                    ImmutableHashSet.Create("54321", "Hello, World", "Bananas")
+                };
+
+                // Distinct Guids
+
+                yield return new object[] {
+                    nameof(FakeImmutable.ImmutableSet),
+                    ImmutableSortedSet.Create(Guid.NewGuid(), Guid.Empty),
+                    ImmutableSortedSet.Create(Guid.Empty, Guid.NewGuid())
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ArePropertyValuesEqual_MismatchedValues_MemberData))]
+        public void ArePropertyValuesEqual_MismatchedValues(
+            String propertyName,
+            IEnumerable leftPropertyValue,
+            IEnumerable rightPropertyValue) {
+
+            // Arrange
+
+            var handler = this.CreateHandler(propertyName);
+            var leftFake = this.NextFake().SetPropertyValue(propertyName, leftPropertyValue);
+            var rightFake = this.NextFake().SetPropertyValue(propertyName, rightPropertyValue);
+
+            // Act
+
+            var areEqual = handler.ArePropertyValuesEqual(leftFake, rightFake);
+
+            // Assert
+
+            Assert.False(areEqual);
+        }
+
+        public static IEnumerable<object[]> ArePropertyValuesEqual_MismatchedSize_MemberData {
+            get {
+
+                // Trailing duplicate on left
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Array),
+                    new object[] { "foo", "bar", "biz" },
+                    new string[] { "foo", "bar" }
+                };
+
+                // Duplicate of 'foo' on right
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Array),
+                    new object[] { "foo", "bar" },
+                    new string[] { "foo", "bar", "foo" }
+                };
+
+                // Trailing duplicate on right
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Array),
+                    new object[] { "foo", "bar" },
+                    new string[] { "foo", "bar", "biz" }
+                };
+
+                // Leading duplicate on right
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Enumerable),
+                    ImmutableList.Create<object>(String.Empty, 45m, DateTime.MinValue),
+                    new object[] { String.Empty, String.Empty, 45m, DateTime.MinValue }
+                };
+
+                // Jagged on left
+
+                yield return new object[] {
+                    nameof(FakeImmutable.Set),
+                    new HashSet<string> { "12345", "Hello, World", "Bananas", "Apples" },
+                    ImmutableSortedSet.Create<string>("12345", "Hello, World", "Bananas" )
+                };
+
+                // Jagged on right
+
+                var guid = Guid.NewGuid();
+
+                yield return new object[] {
+                    nameof(FakeImmutable.ImmutableSet),
+                    ImmutableHashSet.Create(Guid.Empty, guid),
+                    ImmutableSortedSet.Create(Guid.NewGuid(), Guid.Empty, guid)
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ArePropertyValuesEqual_MismatchedSize_MemberData))]
+        public void ArePropertyValuesEqual_MismatchedSize(
+            String propertyName,
+            IEnumerable leftPropertyValue,
+            IEnumerable rightPropertyValue) {
+
+            // Arrange
+
+            var handler = this.CreateHandler(propertyName);
+            var leftFake = this.NextFake().SetPropertyValue(propertyName, leftPropertyValue);
+            var rightFake = this.NextFake().SetPropertyValue(propertyName, rightPropertyValue);
+
+            // Act
+
+            var areEqual = handler.ArePropertyValuesEqual(leftFake, rightFake);
+
+            // Assert
+
+            Assert.False(areEqual);
+        }
+
+        protected override PropertyInfo NextValidPropertyInfo() {
+            return
+                properties
+                    .Skip(random.Next(0, properties.Count))
+                    .First();
+        }
+
+        protected override object NextParent() {
+            return this.NextFake();
+        }
+
+        private FakeImmutable NextFake() {
+            return new FakeImmutable(
+                new object[] { new object() },
+                ImmutableList.Create(DateTime.UtcNow),
+                new HashSet<string> { Guid.NewGuid().ToString(), Guid.NewGuid().ToString() },
+                ImmutableHashSet.Create(Guid.NewGuid())
+            );
+        }
+
+        private IPropertyHandler CreateHandler(string propertyName) {
+            return this.CreateHandler(typeof(FakeImmutable).GetProperty(propertyName));
+        }
+
+        protected override IPropertyHandler CreateHandler(PropertyInfo property) {
+            return new SetPropertyHandler(property);
+        }
+
+        private sealed class FakeImmutable : ImmutableBase<FakeImmutable> {
+
+            public object[] Array { get; }
+            public IEnumerable Enumerable { get; }
+            public ISet<string> Set { get; }
+            public IImmutableSet<Guid> ImmutableSet { get; }
+
+            public FakeImmutable(
+                object[] array = default(object[]),
+                IEnumerable enumerable = default(IEnumerable),
+                ISet<string> set = default(ISet<string>),
+                IImmutableSet<Guid> immutableSet = default(IImmutableSet<Guid>)) {
+
+                this.Array = array;
+                this.Enumerable = enumerable;
+                this.Set = set;
+                this.ImmutableSet = immutableSet;
+            }
+
+            public FakeImmutable SetPropertyValue(String propertyName, object propertyValue) {
+                return this.SetPropertyValueImpl(propertyName, propertyValue);
+            }
+
+        }
+
+    }
+
+}
